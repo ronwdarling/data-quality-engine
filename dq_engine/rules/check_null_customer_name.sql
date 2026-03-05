10b02f34af27b91a2e492ec74112836913029d81
Ron Darling <ronwdarling@gmail.com>
2026-03-05 16:23:02 -0500

    Initializing DQ repo

---
+++ dq_engine/rules/check_null_customer_name.sql
@@ -0,0 +1,7 @@
+-- RULE: Check for NULL customer_name values
+-- Purpose: Ensure customer_name is always populated for analytics
+
+SELECT 
+    COUNT(*) AS null_customer_name_count
+FROM customers
+WHERE customer_name IS NULL:
